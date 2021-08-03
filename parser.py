01ef45bcbbaa2c7368530f14bf8933352695cfaf
martinr241 <martin.roussev@gmail.com>
2021-08-03 14:02:38 +0200

    test changes to parser.py

--- parser.py
+++ parser.py
@@ -2,10 +2,12 @@ import numpy as np
 import os
 
 filename = "benzene.smol";
-def read(filename, directory = None):
+
+
+def read(filename, directory=None):
     if directory:
-        filename = os.path.join(directory, filename);
-        
+        filename = os.path.join(directory, filename)
+
     with open(filename, "r") as f:
         atoms = []
         coordinates = []
@@ -14,17 +16,22 @@ def read(filename, directory = None):
         for l in f.readlines():
             l = l.split()
             if filename.endswith(".mol"):
-               condition_coordinates = len(l) > 14;
-               condition_bond = len(l) > 5 and len(l) < 9;
+                condition_coordinates = len(l) > 14
+                condition_bond = len(l) > 5 and len(l) < 9
             elif filename.endswith(".smol"):
-                condition_coordinates = len(l) == 4;
-                condition_bond = len(l) == 3;
+                condition_coordinates = len(l) == 4
+                condition_bond = len(l) == 3
             if condition_coordinates:
-                coordinates.append([float(i) for i in l[:3]]);
-                atoms.append(l[3]);
+                coordinates.append([float(i) for i in l[:3]])
+                atoms.append(l[3])
             elif condition_bond:
                 bonds.append([int(i) for i in l[:2]])
                 orders.append(int(l[2]))
-    return atoms, coordinates, bonds;
+    return atoms, coordinates, bonds
 
-print(read(filename))
+
+atoms, coordinates, bonds = read(filename, "structures")
+print(atoms)
+print(coordinates)
+print(bonds)
+
